6d6fd6c5bed46e07c399610f18dbcdd5ea8bfd4c
paranoia1906 <anthonymledesma@gmail.com>
2018-07-08 14:51:30 -0700

    Added tracking for last edited date, Added notes area and tracking for the area.

--- src/index.js
+++ src/index.js
@@ -41,6 +41,7 @@ function ToDoItems () {
         description: '',
         dueDate: '',
         createdDate: '',
+        editedDate: 'N/A',
         priority: 0,
         notes: '',
         checklist: ''
@@ -56,25 +57,20 @@ $('#GenerateButton').click(function() {
     x.title = $('#ItemName').val();
     x.description = $('#ItemDescription').val();
 
-    x.createdDate = format(new Date(), 'MM/DD/YYYY H A');
+    x.createdDate = format(new Date(), 'MM/DD/YYYY h A');
     
     
     ToDoArray.push(x);
-    // var result = format(
-//     new Date(2014, 1, 11),
-//     'MM/DD/YY'
-//   )
 
-    
     localStorage.setItem('ToDoArray', JSON.stringify(ToDoArray));
     $('#NewFormDiv').hide();
-    Render();
+    Render(ToDoArray);
 })
 
 const Render = (ToDoArray) => {
     let parentClass = "uk-grid-small uk-child-width-expand@s uk-text-center uk-flex";
     let bodyClass = "uk-card uk-card-default uk-card-body uk-card-hover uk-margin";
-    let badgeClass = "uk-card-badge uk-label";
+    let badgeClass = "uk-card-badge";
     let gridClass = "uk-grid-small uk-child-width-expand@s uk-text-center uk-flex";
     let editClass = "uk-button uk-button-text uk-align-right";
     let stopEditClass = "uk-button uk-button-text uk-align-left";
@@ -83,14 +79,16 @@ const Render = (ToDoArray) => {
         
         $('#ToDoList').append(`
         <div class="${gridClass}">
-            <div data-index="" >
+            <div>
                 <div class="${parentClass}">
                     <div class="${bodyClass}">
-                        <div id="date-badge-${index}" class="${badgeClass}">${element.createdDate}</div>
+                        <div id="date-badge-${index}" class="${badgeClass}">Created: ${element.createdDate}<br />Edited: ${element.editedDate}</div>
                         <h3 id="item-title-${index}" class="uk-card-title">${element.title}</h3>
                             <input type="text" id="ItemName${index}" value="${element.title}"><br class="spacing" />
                         <p id="description-tag-${index}">${element.description}</p>
                             <textarea id="ItemDescription${index}">${element.description}</textarea><br class="spacing" />
+                        <p id="notes-tag-${index}">${element.notes}</p>
+                            <textarea id="ItemNotes${index}">${element.notes}</textarea><br class="spacing" />
                         <div id="edit-${index}" class="${editClass}">Edit</div>
                         <div id="stop-edit-${index}" class="${stopEditClass}">Stop Editing</div>
                     </div>
@@ -109,6 +107,9 @@ const Render = (ToDoArray) => {
         $(`#ItemDescription${index}`).hide();
         $(`#ItemName${index}`).hide();
         $(`.spacing`).hide();
+        $(`#ItemNotes${index}`).hide();
+        
+        
 
 
         $(`#stop-edit-${index}`).click(function() {
@@ -145,6 +146,8 @@ const EditMode = (index) =>{
     $(`#description-tag-${index}`).hide();
     $(`#ItemDescription${index}`).show();
 
+    $(`#ItemNotes${index}`).show();
+    $(`#notes-tag-${index}`).hide();
 }
 
 const EndEditMode = (index,arr) => {
@@ -162,12 +165,19 @@ const EndEditMode = (index,arr) => {
     arr[index].title = $(`#item-title-${index}`).html();
 
     $(`#description-tag-${index}`).show();
-    $(`#description-tag-${index}`).html($(`#ItemDescription${index}`).html());
+    $(`#description-tag-${index}`).html($(`#ItemDescription${index}`).val());
     arr[index].description = $(`#description-tag-${index}`).html();
     $(`#ItemDescription${index}`).hide();
 
-    localStorage.setItem('ToDoArray', JSON.stringify(arr));
+    $(`#ItemNotes${index}`).hide();
+    $(`#notes-tag-${index}`).show();
+    $(`#notes-tag-${index}`).html($(`#ItemNotes${index}`).val());
+    arr[index].notes = $(`#ItemNotes${index}`).val();
 
+    arr[index].editedDate = format(new Date(), 'MM/DD/YYYY h A');
+
+    localStorage.setItem('ToDoArray', JSON.stringify(arr));
+    Render(ToDoArray);
 }
 
 
