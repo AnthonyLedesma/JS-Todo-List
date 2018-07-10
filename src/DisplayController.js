19ea218ded598993d9eb96d311f27a25c28fbdfd
paranoia1906 <anthonymledesma@gmail.com>
2018-07-10 09:05:41 -0700

    Separated the Display Controller code into seperate module. Modal controls are now in place. Inputs are sanitized.

--- src/DisplayController.js
+++ src/DisplayController.js
@@ -1,12 +1,95 @@
+// Whole-script strict mode syntax
+'use strict';
 const $ = require('jquery');
 //const jQuery = require('jquery');
 
 $('#ToDoList').append("<li>This item is ToDo</li>");
 $('#DoneList').append("<li>This item is done</li>");
 
+//import ToDoArray from 'index.js';
+import DOMPurify from 'dompurify';
 
 $('#NewToDo').click(function() {
     $('#NewFormDiv').show();
 
 })
 
+
+
+const render = (ToDoArray) => {
+    let parentClass = "uk-grid-small uk-child-width-expand@s uk-text-center uk-flex";
+    let badgeClass = "uk-card-badge";
+    let bodyPriorityClass = '';
+    let gridClass = "uk-grid-small uk-child-width-expand@s uk-text-center uk-flex";
+    let editClass = "uk-button uk-button-text uk-align-right";
+
+    preRenderClear();
+    ToDoArray.forEach((element,index) => {
+
+        if (parseInt(element.priority) === 0) {bodyPriorityClass = "uk-card uk-card-default uk-card-body uk-card-hover uk-margin";}
+        else if (parseInt(element.priority) === 1) {bodyPriorityClass = "uk-card uk-card-primary uk-card-body uk-card-hover uk-margin";}
+        else if (parseInt(element.priority) === 2) {bodyPriorityClass = "uk-card uk-card-secondary uk-card-body uk-card-hover uk-margin";}
+        $('#ToDoList').append(`
+        <div class="${gridClass}">
+            <div>
+                <div class="${parentClass}">
+                    <div class="${bodyPriorityClass}">
+                        <div id="date-badge-${index}" class="${badgeClass}">Created: ${element.createdDate}<br />Edited: ${element.editedDate}</div>
+                        <h3 id="item-title-${index}" class="uk-card-title">${element.title}</h3>
+                        <p id="description-tag-${index}">${element.description}</p>
+                        <p id="notes-tag-${index}">${element.notes}</p>
+                        <p id="priority-tag-${index}">Priority ${element.priority}</p>
+                        <div id="edit-${index}" class="${editClass}" uk-toggle="target: #modal${index}">Edit</div>
+                    </div>
+                </div>
+            </div>
+        </div>`
+        );
+        
+        $('body').append(`<div data-modal='true' id="modal${index}" uk-modal><div class="uk-modal-dialog uk-modal-body">
+            <h2 class="uk-modal-title">${element.title}</h2>
+            <span class="uk-label">Title</span><input class="uk-form-small uk-input" type="text" id="ItemName${index}" value="${element.title}">
+            <span class="uk-label">Description</span><textarea class="uk-textarea" id="ItemDescription${index}">${element.description}</textarea>
+            <span class="uk-label">Notes</span><textarea class="uk-textarea" id="ItemNotes${index}">${element.notes}</textarea>
+            <span class="uk-label">Priority</span><div class="uk-margin uk-grid-small uk-child-width-auto uk-grid">
+                <label><input class="uk-radio" type="radio" name="radio${index}" data-value="0"> Low</label>
+                <label><input class="uk-radio" type="radio" name="radio${index}" data-value="1"> Normal</label>
+                <label><input class="uk-radio" type="radio" name="radio${index}" data-value="2"> High</label>
+            </div>
+            <p class="uk-text-right"><button id="save${index}" class="uk-button uk-button-primary" type="button">Save</button></p></div></div>
+        `);
+        
+        
+        $(`#save${index}`).click(function(e) {
+            
+            //e.target.parentNode.parentNode.parentNode.remove();
+            UIkit.modal(e.target.parentNode.parentNode.parentNode).hide();
+            $('html').removeAttr('class');
+            $('body').removeAttr('style');
+            ToDoArray = editMode(index,ToDoArray); 
+            render(ToDoArray);
+        });
+
+        $(`input[name="radio${index}"][data-value="${parseInt(element.priority)}"]`).prop('checked', true);
+ 
+    });
+}
+
+const editMode = (index,arr) => {
+
+    arr[index].title = DOMPurify.sanitize($(`#ItemName${index}`).val(), {ALLOWED_TAGS: ['b']});
+    arr[index].description = DOMPurify.sanitize($(`#ItemDescription${index}`).val(), {ALLOWED_TAGS: ['b']});
+    arr[index].notes = DOMPurify.sanitize($(`#ItemNotes${index}`).val(), {ALLOWED_TAGS: ['b']});
+    arr[index].editedDate = format(new Date(), 'MM/DD/YYYY h A');
+    arr[index].priority = $(`input[name="radio${index}"]:checked`).attr('data-value');
+
+    localStorage.setItem('ToDoArray', JSON.stringify(arr));
+    return arr;
+}
+
+const preRenderClear = () => {
+    $('#ToDoList').empty();
+    $('div[data-modal="true"]').remove();
+}
+
+export {render};
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 // Whole-script strict mode syntax
 'use strict';
 const $ = require('jquery');
+
 require("expose-loader?$!jquery");
 
-import './DisplayController';
+import {render} from './DisplayController.js';
 
 
 import UIkit from 'uikit';
@@ -50,86 +51,10 @@ $('#GenerateButton').click(function() {
     ToDoArray.push(x);
     localStorage.setItem('ToDoArray', JSON.stringify(ToDoArray));
     $('#NewFormDiv').hide();
-    Render(ToDoArray);
+    render(ToDoArray);
 })
 
-const Render = (ToDoArray) => {
-    let parentClass = "uk-grid-small uk-child-width-expand@s uk-text-center uk-flex";
-    let badgeClass = "uk-card-badge";
-    let bodyPriorityClass = '';
-    let gridClass = "uk-grid-small uk-child-width-expand@s uk-text-center uk-flex";
-    let editClass = "uk-button uk-button-text uk-align-right";
-
-    $('#ToDoList').empty();
-    ToDoArray.forEach((element,index) => {
-        console.log(`the priority of the element is ${element.priority}`);
-        if (parseInt(element.priority) === 0) {bodyPriorityClass = "uk-card uk-card-default uk-card-body uk-card-hover uk-margin";}
-        else if (parseInt(element.priority) === 1) {bodyPriorityClass = "uk-card uk-card-primary uk-card-body uk-card-hover uk-margin";}
-        else if (parseInt(element.priority) === 2) {bodyPriorityClass = "uk-card uk-card-secondary uk-card-body uk-card-hover uk-margin";}
-        $('#ToDoList').append(`
-        <div class="${gridClass}">
-            <div>
-                <div class="${parentClass}">
-                    <div class="${bodyPriorityClass}">
-                        <div id="date-badge-${index}" class="${badgeClass}">Created: ${element.createdDate}<br />Edited: ${element.editedDate}</div>
-                        <h3 id="item-title-${index}" class="uk-card-title">${element.title}</h3>
-                        <p id="description-tag-${index}">${element.description}</p>
-                        <p id="notes-tag-${index}">${element.notes}</p>
-                        <p id="priority-tag-${index}">Priority ${element.priority}</p>
-                        <div id="edit-${index}" class="${editClass}" uk-toggle="target: #modal${index}">Edit</div>
-                    </div>
-                </div>
-            </div>
-        </div>`
-        );
-        
-        $('#ToDoList').append(`<div id="modal${index}" uk-modal><div class="uk-modal-dialog uk-modal-body">
-            <h2 class="uk-modal-title">${element.title}</h2>
-            <span class="uk-label">Title</span><input class="uk-form-small uk-input" type="text" id="ItemName${index}" value="${element.title}">
-            <span class="uk-label">Description</span><textarea class="uk-textarea" id="ItemDescription${index}">${element.description}</textarea>
-            <span class="uk-label">Notes</span><textarea class="uk-textarea" id="ItemNotes${index}">${element.notes}</textarea>
-            <span class="uk-label">Priority</span><div class="uk-margin uk-grid-small uk-child-width-auto uk-grid">
-                <label><input class="uk-radio" type="radio" name="radio${index}" data-value="0"> 0</label>
-                <label><input class="uk-radio" type="radio" name="radio${index}" data-value="1"> 1</label>
-                <label><input class="uk-radio" type="radio" name="radio${index}" data-value="2"> 2</label>
-            </div>
-            <p class="uk-text-right"><button id="save${index}" class="uk-button uk-button-primary uk-modal-close" type="button">Save</button></p></div></div>
-        `);
-        $(`input[name="radio${index}"][data-value="${element.priority}"]`).prop('checked', true);
-        $(`#save${index}`).click(function() {
-            console.log("Here we have save button click");
-            editMode(index,ToDoArray); 
-        });
-
-
-    });
-}
-Render(ToDoArray);
-
-
-
-const editMode = (index,arr) => {
-
-
-    $(`#item-title-${index}`).html($(`#ItemName${index}`).val());
-    arr[index].title = $(`#item-title-${index}`).html();
-
-    $(`#description-tag-${index}`).html($(`#ItemDescription${index}`).val());
-    arr[index].description = $(`#description-tag-${index}`).html();
-
-
-    $(`#notes-tag-${index}`).html($(`#ItemNotes${index}`).val());
-    arr[index].notes = $(`#ItemNotes${index}`).val();
-
-    arr[index].editedDate = format(new Date(), 'MM/DD/YYYY h A');
-    
-   
-    $(`#priority-tag-${index}`).html($(`input[name="radio${index}"]:checked`).attr('data-value'));
-    arr[index].priority = $(`#priority-tag-${index}`).html();
-
-    localStorage.setItem('ToDoArray', JSON.stringify(arr));
-    Render(arr);
-}
+render(ToDoArray);
 
 
 
